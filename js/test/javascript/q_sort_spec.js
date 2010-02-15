c88dde2e1fca8fb1c56db71efd4654cbe09a1950
Dmitry Ratnikov <ratnikov@gmail.com>
2010-02-15 05:19:25 -0600

    Generated blue ridge and added dummy qSort stuff

---
+++ js/test/javascript/q_sort_spec.js
@@ -0,0 +1,10 @@
+require("spec_helper.js");
+require("../../public/javascripts/q_sort.js");
+
+Screw.Unit(function(){
+  describe("QSort", function(){
+    it("does something", function(){
+      expect("hello").to(equal, "hello");
+    });
+  });
+});
